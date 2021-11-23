b12cdc9a1da14931674513c46a5a1e58d296b929
hhavnoor <94912206+hhavnoor@users.noreply.github.com>
2021-11-23 19:36:26 +0530

    Add files via upload

---
+++ features/step-definitions/product.step.js
@@ -0,0 +1,12 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+import productPage from "../pageobjects/product.page";
+
+Given(/^I add below products to cart$/, async (table) => {
+  await productPage.addProducts(table);
+});
+
+Then(/^I shall validate shopping cart as below$/, async (table) => {
+  await productPage.verifyShoppingCart(table);
+});
+
+
